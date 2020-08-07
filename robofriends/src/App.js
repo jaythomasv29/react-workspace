184ebf41fac4cfde7416193db9beb939cec84adb
James Vongampai <jaythomasv29@gmail.com>
2020-08-07 14:44:47 -0700

    added state to dynamically pass users object

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import { robots } from './robots'
-const App = () => {
-    return(
-        <div className="tc">
-            <h1 className="f1">Robofriends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-    )
+
+class App extends Component {  //create class to use state
+    constructor(){ //constructor
+        super() // call super on Component class
+        this.state = {
+            robots: robots,
+            searchfield: '',
+        }
+    }
+    render(){
+        return(
+            <div className="tc">
+                <h1 className="f1">Robofriends</h1>
+                <SearchBox/>
+                <CardList robots={this.state.robots}/>
+            </div>
+        )
+    }
 }
 export default App
