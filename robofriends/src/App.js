c02aed44bab84f23263d8f09deef49020408f359
James Vongampai <jaythomasv29@gmail.com>
2020-08-09 13:11:31 -0700

    changed app component to dynamically load data using http request

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
 import './App.css';
 
 class App extends Component {  //create class to use state
     constructor(){ //constructor
+        // console.log('constructor')
         super() // call super on Component class
         this.state = {  //something that can change and effect our app - STATE lives in parent component and passes to smaller components
             robots: [],  //App component has TWO STATES 'robots' and searchfield'
@@ -13,10 +13,20 @@ class App extends Component {  //create class to use state
         }
     }
 
+    //componentDidMount Lifecycle method
+    componentDidMount() {
+        //http request
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}))
+            
+    }
+
     onSearchChange = (event) => {  //your own methods must be in arrow functions
         this.setState({ searchfield: event.target.value })  //set state using .setState to change searchfield
     }
     render(){
+        // console.log('render')
         const filteredRobots = this.state.robots.filter(robot => {
                 return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
